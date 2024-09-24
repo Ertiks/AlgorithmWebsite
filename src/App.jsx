499d0b4cd97ed489a653e1c39bb889a6ca125fb6
Ertiks <ertiks42@yahoo.fr>
2024-09-24 17:16:45 +0200

    Basic Routing added

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,45 @@ import Footer from "./components/FooterComponent"
 import Header from "./components/HeaderComponent"
 import MainContent from './components/MainContentComponent';
 
+import { createBrowserRouter, RouterProvider , Link} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path : "/",
+    element: 
+      <div>
+        <nav>
+          <Link to="/blog"> Blog </Link>
+          <Link to="/"> Accueil </Link>
+        </nav>
+        Page d'accueil 
+      </div>
+  },
+  {
+    path : "/blog",
+    element: 
+    <div>
+      <nav>
+        <Link to="/blog"> Blog </Link>
+        <Link to="/"> Accueil </Link>
+      </nav>
+        Page 1 
+    </div>
+  }
+])
 
 function App() {
-  return (
-    <div className='mainPage'>
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
-  );
+
+  return <RouterProvider router={router} />
+
+  //OLD APP :
+  // return (
+  //   <div className='mainPage'>
+  //     <Header />
+  //     <MainContent />
+  //     <Footer />
+  //   </div>
+  // );
 }
 
 export default App
